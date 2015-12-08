f942059dcc4f01b17bc02c8bef129db8b4f5a9dc
Anh Nguyen <khacanh@gmail.com>
2015-12-08 14:11:33 +0700

    Update test requirements

---
+++ app/src/main/java/com/mysquar/mychat/App.java
@@ -0,0 +1,21 @@
+package com.mysquar.mychat;
+
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by anhnguyen on 12/8/15.
+ */
+public class App extends Application {
+
+    public static final String FIREBASE_URL = "https://mysquar-test.firebaseio.com/chat";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Firebase.setAndroidContext(this);
+        new Firebase(FIREBASE_URL);
+    }
+}
