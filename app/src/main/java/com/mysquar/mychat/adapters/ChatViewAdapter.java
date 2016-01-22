67fc67e00482975c6f5f949f66601a99982657c1
Hieu Tran <tr.kimhieu@gmail.com>
2016-01-22 15:07:34 +0700

    Implement basic getView()

--- app/src/main/java/com/mysquar/mychat/adapters/ChatViewAdapter.java
+++ app/src/main/java/com/mysquar/mychat/adapters/ChatViewAdapter.java
@@ -1,11 +1,14 @@
 package com.mysquar.mychat.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
+import android.widget.TextView;
 
+import com.mysquar.mychat.R;
 import com.mysquar.mychat.models.ChatItem;
 
 import java.util.List;
@@ -15,12 +18,20 @@ import java.util.List;
  */
 public class ChatViewAdapter extends ArrayAdapter<ChatItem> {
 
+    private Context context;
+
     public ChatViewAdapter(Context context, int resource, List<ChatItem> objects) {
         super(context, resource, objects);
+        this.context = context;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.cell_chat_item, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.text_chat_content);
+
+        return rowView;
     }
 }
