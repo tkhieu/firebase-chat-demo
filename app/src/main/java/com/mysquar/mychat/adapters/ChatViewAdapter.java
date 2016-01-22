77f7eebb61307c9750732aabbd9fe1295de7d0a3
Hieu Tran <tr.kimhieu@gmail.com>
2016-01-22 15:03:24 +0700

    Add getView to ChatViewAdapter and create cell_chat_item

--- app/src/main/java/com/mysquar/mychat/adapters/ChatViewAdapter.java
+++ app/src/main/java/com/mysquar/mychat/adapters/ChatViewAdapter.java
@@ -1,6 +1,8 @@
 package com.mysquar.mychat.adapters;
 
 import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListAdapter;
 
@@ -15,5 +17,10 @@ public class ChatViewAdapter extends ArrayAdapter<ChatItem> {
 
     public ChatViewAdapter(Context context, int resource, List<ChatItem> objects) {
         super(context, resource, objects);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return super.getView(position, convertView, parent);
     }
 }
