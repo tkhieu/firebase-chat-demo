2f18c1b8abde0460049323cc501457127d792d55
Hieu Tran <tr.kimhieu@gmail.com>
2016-07-14 14:07:02 +0700

    Add facebook login

--- app/src/main/java/com/mysquar/chat/App.java
+++ app/src/main/java/com/mysquar/chat/App.java
@@ -2,13 +2,10 @@ package com.mysquar.chat;
 
 import android.app.Application;
 
-
-import com.google.firebase.database.FirebaseDatabase;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.mysquar.chat.helpers.FirebaseHelper;
 
-/**
- * Created by anhnguyen on 12/8/15.
- */
 public class App extends Application {
 
     @Override
@@ -16,6 +13,7 @@ public class App extends Application {
         super.onCreate();
         AppConfig.getInstance();
         FirebaseHelper.getInstance();
-
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this);
     }
 }
